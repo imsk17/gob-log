2ca9476142c5498d94490f782435b39bc7ab3e98
imsk17 <imsumitkumar17@gmail.com>
2021-05-03 22:35:36 +0530

    :building_construction: Initial Setup For Fiber

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"embed"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
+	"log"
+	"net/http"
+)
+
+// Embed the static/ directory to the app to serve favicons, custom css
+// and all other `static` stuff.
+//go:embed static
+var static embed.FS
 
 func main() {
-	fmt.Print("Hello World")
+	// Create a New Fiber App.
+	// TODO: Use the `http` package instead.
+	app := fiber.New()
+
+	// Mount the static directory
+	app.Use("/", filesystem.New(
+		filesystem.Config{
+			Root:   http.FS(static),
+			Browse: false,
+		}))
+
+	// Start the fiber app woo-hoo.
+	log.Panic(app.Listen(":4000"))
 }
