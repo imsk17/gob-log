e8a4497487d4581b0e3007f822adf45d876c5978
imsk17 <imsumitkumar17@gmail.com>
2021-05-03 22:46:35 +0530

    :art: Setup Initial Templating

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
+	"github.com/gofiber/template/html"
 	"log"
 	"net/http"
 )
@@ -13,10 +14,27 @@ import (
 //go:embed static
 var static embed.FS
 
+// Embed the Template Library
+//go:embed templates/*
+var template embed.FS
+
 func main() {
+
+	// Setup a Template Engine
+	engine := html.NewFileSystem(http.FS(template), ".html")
+
 	// Create a New Fiber App.
 	// TODO: Use the `http` package instead.
-	app := fiber.New()
+	app := fiber.New(
+		fiber.Config{
+			Views: engine,
+		})
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.Render("templates/index", fiber.Map{
+			"Title": "again, mate.",
+		})
+	})
 
 	// Mount the static directory
 	app.Use("/", filesystem.New(
