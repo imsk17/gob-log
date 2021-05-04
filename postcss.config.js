13e1a05f2b39d03aaaa85a73d94b56b8947a4d83
imsk17 <imsumitkumar17@gmail.com>
2021-05-04 11:49:56 +0530

    :sparkles: Tailwind: Setup
    Spit out the smallest css possible

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+        './templates/**/*.html',
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+const cssnano = require("cssnano")({
+    preset: "default",
+})
+
+module.exports = {
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+        ...(process.env.NODE_ENV === "production"
+            ? [
+                purgecss,
+                cssnano
+            ]
+            : []),
+    ]
+}
