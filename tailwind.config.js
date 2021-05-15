abbc0118e7a1bb4aa06a13ab5c42131d42d5642f
imsk17 <imsumitkumar17@gmail.com>
2021-05-15 22:32:21 +0530

    :art: Bring in colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     './templates/*.html',
   ],
   darkMode: false, // or 'media' or 'class'
-  theme: {},
+  theme: {
+    colors: {
+      primarybg: '#1E1F21',
+      secondarybg: '#292A2E',
+      pink: '#8D6F95',
+      yellow: '#F7D365',
+      blue: '#819DB5',
+      white: '#FFFFFF',
+      green: '#B9C36A',
+      red: '#B8505B'
+    }
+  },
   corePlugins: {
     preflight: false
   },
